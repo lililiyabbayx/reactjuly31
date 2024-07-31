3932dbd9aec095d52de93a82a359259f9e6fb4d0
lililiyabbayx <farhinulfatanikaa@gmail.com>
2024-07-31 20:58:25 +0600

    first

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import ListGroup from "./components/ListGroup";
+function App() {
+  let items = ["A", "B", "C", "D"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
+}
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,7 @@
+ //PascalCasing
+ function Message() {
+    //JSX
+    return <h1> Hello world</h1>;
+ }
+ export default Message;
+ 
